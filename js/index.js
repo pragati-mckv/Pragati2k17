e0c3ccdff72f4b3bb49b1f652ac55c361f92232f
deboGunner <033debo033@gmail.com>
2017-05-02 23:42:24 +0530

    Second fix

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-toggle=0;
+	toggle=0;
 		function hidefooter(){
 			if(toggle==0){
 				toggle=1;
-				$('body').css('overflowY', 'auto');
+				$('body').css('overflow-y', 'auto');
 			}else{
 				toggle=0;
-				$('body').css('overflowY', 'auto');
+				$('body').css('overflow-y', 'hidden');
 			}
 		}
